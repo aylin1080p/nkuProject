95d72eb1df6721f4eae8ad02c31b594b78ecca9a
GunesAylin <81433838+GunesAylin@users.noreply.github.com>
2021-10-28 18:38:17 +0300

    Initial commit v1.0

---
+++ app/src/main/java/com/e/nkuproject/Add_Event_Activity.java
@@ -0,0 +1,14 @@
+package com.e.nkuproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Event_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+    }
+}
---
+++ app/src/main/java/com/e/nkuproject/Login_Activity.java
@@ -0,0 +1,33 @@
+package com.e.nkuproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Login_Activity extends AppCompatActivity {
+
+    private TextView btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login_Activity.this,Feed_Activity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+
+    }
+}
